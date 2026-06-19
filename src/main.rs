d8a4f840e29bc0abd21b0014cfb034ee01f46abd
aesopdev <smam@Titanic.local>
2026-06-19 20:24:42 +0400

    Generate first image :)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
-use std::io::Cursor;
-use image::ImageReader;
+// use std::io::Cursor;
+// use image::ImageReader;
 
 fn main() {
-    let int img_width = 256;
-    let int img_height = 256;
+    // image
+    let img_width:  i32 = 256;
+    let img_height: i32 = 256;
+
+    // render
+    println!("P3\n{}{}{}{}", img_width, " ", img_height, "\n255\n");
+
+    for j in 0..img_height {
+        for i in 0..img_width {
+            let r = (i as f64) /  (img_width as f64 - 1.0);
+            let g = (j as f64) / (img_height as f64 - 1.0);
+            let b = 0.0;
+
+            let ir: i32 = (255.999 * r) as i32;
+            let ig: i32 = (255.999 * g) as i32;
+            let ib: i32 = (255.999 * b) as i32;
+
+            println!("{}{}{}{}{}{}", ir, ' ', ig, ' ', ib, '\n');
+        } 
+    }
 }
