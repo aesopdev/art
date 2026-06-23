54ced00e5f09b460c3c5cf932c36962f6f08d48a
aesopdev <118890467+aesopdev@users.noreply.github.com>
2026-06-23 20:23:34 +0400

    Two types of circles with gradients

--- src/main.rs
+++ src/main.rs
@@ -12,24 +12,32 @@ use crate::vec3::dot;
 use crate::vec3::unit_vector;
 use vec3::Vec3;
 
-fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> bool {
+fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> f64 {
     let oc: Vec3 = center - r.origin();
     let a = dot(r.direction(), r.direction());
     let b = -2.0 * dot(oc, oc) - radius * radius;
     let c = dot(oc,oc) - radius * radius;
 
     let discriminant = b * b - 4.0 * a * c;
-    discriminant >= 0.0
+
+    if discriminant < 0.0 {
+        return -1.0;
+    } else {
+        // return -b - discriminant.sqrt() / 2.0 * a; TODO: try this at end instead of below line
+        return (-b - discriminant.sqrt()) / (2.0 * a);
+    }
 }
 
 fn ray_color(r: &Ray) -> Color {
-    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
-        return Color::new(1.0, 0.0, 0.0);
+    let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0 {
+        let n = unit_vector(r.at(t) - Vec3::new(0.0, 0.0, -1.0));
+        return 0.5 * Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
     }
 
     let unit_direction: Vec3 = unit_vector(r.direction());
     let a = 0.5 * (unit_direction.y() + 1.0);
-    (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
+    return (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0);
 }
 
 fn main() {
