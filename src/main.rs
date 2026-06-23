9c07671339c3b82b7c67a5a89a5334b8e3427f08
aesopdev <118890467+aesopdev@users.noreply.github.com>
2026-06-23 20:08:40 +0400

    Red circle generating

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,25 @@ use crate::ray::Ray;
 use crate::color::write_color;
 use crate::color::Color;
 use crate::vec3::Point3;
+use crate::vec3::dot;
 use crate::vec3::unit_vector;
 use vec3::Vec3;
 
+fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> bool {
+    let oc: Vec3 = center - r.origin();
+    let a = dot(r.direction(), r.direction());
+    let b = -2.0 * dot(oc, oc) - radius * radius;
+    let c = dot(oc,oc) - radius * radius;
+
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant >= 0.0
+}
+
 fn ray_color(r: &Ray) -> Color {
+    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
+
     let unit_direction: Vec3 = unit_vector(r.direction());
     let a = 0.5 * (unit_direction.y() + 1.0);
     (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
